8e1d7b5cc2e0c45bba2e8d51591e96c4a0b4c628
mari <msheibley@gmail.com>
2014-01-20 13:32:38 -0500

    Font changes

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.img_container').mouseenter(function() {
+        $('.img_container').fadeTo('fast', 1)
+    $('header').mouseleave(function() {
+        $('header').fadeTo('fast', 0.5);
+            })
+    })
+})
