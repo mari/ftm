4a63f700cc99afad0c0b04bfd2ef1fea8ce4085d
mari <msheibley@gmail.com>
2014-02-10 17:32:22 -0500

    new js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
-    $('.img_container').mouseenter(function() {
-        $('.img_container').fadeTo('fast', 1)
-    $('header').mouseleave(function() {
-        $('header').fadeTo('fast', 0.5);
-            })
-    })
-})
+  $('header').slideDown("slow")  
+});
